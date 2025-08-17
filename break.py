2ae604f05e0a4986badd6a78de05ecd5e428345e
Jyothi  Edumudi <159909807+edumudijyothi@users.noreply.github.com>
2025-08-17 22:07:07 +0530

    Update break.py

--- break.py
+++ break.py
@@ -1,6 +1,15 @@
 for i in range(1,6):
     if i==3:
         break
-        print(i)
-    
-    
+    print(i)
+'''    
+0
+1
+2
+3
+    '''
+for i in range(1,6):
+    if i==3:
+        break
+print(i)
+'''4'''
