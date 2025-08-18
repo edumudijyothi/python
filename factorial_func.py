3c669a3bd66a176768e6867f747cae0f760f92b4
Jyothi  Edumudi <159909807+edumudijyothi@users.noreply.github.com>
2025-08-18 12:28:38 +0530

    Update factorial_func.py

--- factorial_func.py
+++ factorial_func.py
@@ -4,5 +4,13 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
-    
 print(factorial(4)) 
+
+
+#without factorial
+def fact(n):
+    result=1
+    for i in range(1,n+1):
+        result*=i
+    return result
+print(fact(4))    '''4! = 4 x 3 × 2 × 1=24'''
